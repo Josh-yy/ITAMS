d11426119bcd0ae2a0c591af4a748fa64de6be91
Josh-yy <126300620+Josh-yy@users.noreply.github.com>
2025-01-30 19:09:42 +0800

    delete additional files

--- PHINMA/listviews/passing_rate.php
+++
@@ -1,7 +0,0 @@
-<?php
-@session_start();
-require("../assets/settings/db_conn.php");
-require("../assets/settings/functions.php");
-?>
-
-<h1 class="text-secondary"><?php echo get_column2("passing_rate","select * from t_passing_rate",$db) ?> % </h1>
--- PHINMA/listviews/time_allotment.php
+++
@@ -1,7 +0,0 @@
-<?php
-@session_start();
-require("../assets/settings/db_conn.php");
-require("../assets/settings/functions.php");
-?>
-
-<h1 class="text-secondary"><?php echo get_column2("time_allotment","select * from t_time_allotment",$db) ?> minutes </h1>
